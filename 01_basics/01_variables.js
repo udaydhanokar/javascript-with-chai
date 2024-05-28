6da43e9d679bd7eac2c7df8eb95cc55d0a0d134b
udaydhanokar <139249387+udaydhanokar@users.noreply.github.com>
2024-05-28 12:24:49 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 145525
+let accountEmail="udaydhanokar@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState
+/*prefer not to use var because of issue in block scope and funtional scope*/
+accountEmail="JaiShreeRam@gmail.com"
+console.table([accountId,,accountEmail,accountState,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict" //treat all js code as newer version
+
+//alert(3 + 3) //we are using node js not browser
+
+console.log(3 +
+    
+    3) //code readialble should be high 
+
+    console.log("Hitesh")
+
+    let name = "uday"
+    let age = 21
+    let password = true
+    
+    let state
+    let tempreture = null
+    /*
+       number => 2 to the power 53
+       bigint
+       string => ""
+       boolean = true/false
+       null => standalone value
+       undefined =>
+
+        symbol=> unique
+
+        object
+
+     */
+
+console.log(typeof("uday"))
+
+console.log(typeof(undefined))
+
+console.log(typeof(null))
+
+console.log(typeof("55"))
+
+console.log(typeof(48))
