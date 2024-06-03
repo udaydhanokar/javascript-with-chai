93a112889c88f2867eec293008e612e0276473c0
udaydhanokar <139249387+udaydhanokar@users.noreply.github.com>
2024-06-03 04:18:45 +0000

    01_arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,48 @@
+// let arr = [1,2,3,4]
+// console.log(arr[3])
+
+// let heros=["krish","shaktiman","nagraj"]
+// console.log(heros[2])
+
+// let arr1 = new Array("hi","how","are","you")
+// console.log(arr1[0])
+
+
+
+// console.log(typeof arr1)
+// console.log(typeof arr)
+
+// arr1.push("tata")
+// arr1.push(2)
+// arr1.push(100)
+// arr1.pop()//no need to write anything in () paranthesis because they pop only last value
+
+
+// arr1.unshift(50)
+// arr1.unshift(9)
+// arr1.shift()
+
+// console.log(arr1.includes(2));
+// console.log(arr1.indexOf("are"))
+
+// console.log(arr1);
+
+// const newArr = arr1.join();
+// console.log(newArr)
+// console.log(typeof newArr)
+
+//*****************slice/splice***********
+
+let newArray =[0,1,2,3,4,5]
+
+console.log("A",newArray);
+
+const myn1 = newArray.slice(1,3);
+console.log(myn1);
+
+console.log("B",newArray);
+const myn2 = newArray.splice(1,3)
+console.log(myn2)
+
+console.log("C",newArray);
+
