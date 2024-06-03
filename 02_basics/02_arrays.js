0bf508395553b43bb1f0823f22d3c7aa09b0b23e
udaydhanokar <139249387+udaydhanokar@users.noreply.github.com>
2024-06-03 04:19:10 +0000

    02_arrays.js

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,29 @@
+// let marvel_hero=["ironman","spiderman","captain America"]
+// let dc_hero=["batman","flash","superman"]
+
+// // marvel_hero.push(dc_hero)
+// // console.log(marvel_hero);
+
+// const allheros = marvel_hero.concat(dc_hero)
+// console.log(allheros)
+
+// let allnewHero=[...marvel_hero,...dc_hero]
+// console.log(allnewHero)
+
+//***********************************************
+
+// const newA =[1,2,3,[4,5,6],7,[8, 9,10,[11,12]]]
+// const newAReal =newA.flat(Infinity);
+// console.log(newAReal)
+
+//************************************** */
+
+console.log(Array.isArray("Uday"))
+console.log(Array.from("Uday"))
+console.log(Array.from({name:"Uday"}))//
+
+
+let score1 =1;
+let score2 =2;
+let score3 =3;
+console.log(Array.of(score1,score2,score3))
