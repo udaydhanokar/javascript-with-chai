673495bef961998c49a7d039a5e287e6c68053cf
udaydhanokar <139249387+udaydhanokar@users.noreply.github.com>
2024-06-08 03:38:59 +0000

    02_scope2.js

---
+++ 03_basics/02_scope2.js
@@ -0,0 +1,52 @@
+// function one(value){
+//    let add = value +1;
+//    function two(value1){
+//     console.log(value1)
+//     console.log(add)
+//    }
+// //    console.log(value1)
+//    two(39)
+
+// }
+// one(3)
+
+function one(){
+    const username = "uday"
+    function two(){
+        const website ="youtube"
+        console.log(username)
+    }
+ two()   
+
+//  console.log(website)
+ 
+}
+// one()
+
+
+
+
+if(true){
+    const username="hitesh"
+    if (username==="hitesh"){
+        const website= " youtube";
+        console.log(username+ website)
+    }
+    // console.log(website)
+
+}
+// console.log(username)
+
+/*---------------------------------------------*/
+
+console.log(addone(3))
+function addone(value){
+    return value;
+}
+
+
+console.log(functionStored(2))
+let functionStored=function addtwo(value2){
+    return value2
+}
+
