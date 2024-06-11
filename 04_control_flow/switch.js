ccfe4cbf73cca3cfbe6da3ea05f09353fe788f76
udaydhanokar <139249387+udaydhanokar@users.noreply.github.com>
2024-06-11 18:17:12 +0000

    switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,55 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let month =3
+// switch(month){
+//     case 1:
+//     console.log("january")
+//     break;
+
+//     case 2:
+//     console.log("feb")
+//     break;
+    
+//     case 3:
+//     console.log("march")
+//     break;
+    
+//     case 4:
+//     console.log("april")
+//     break;
+    
+//     default:
+//     console.log("you enter in default section")
+//     break;
+// }
+
+
+let month = "april"
+switch(month){
+    case "jan":
+    console.log("january")
+    break;
+
+    case "feb":
+    console.log("feb")
+    break;
+    
+    case "march":
+    console.log("march")
+    break;
+    
+    case "april":
+    console.log("april")
+    break;
+    
+    default:
+    console.log("you enter in default section")
+    break;
+}
