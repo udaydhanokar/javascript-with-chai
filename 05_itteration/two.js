f1d731b02aa2c20ae28049811e5be89751df90b8
udaydhanokar <139249387+udaydhanokar@users.noreply.github.com>
2024-06-11 18:18:36 +0000

    two.js

---
+++ 05_itteration/two.js
@@ -0,0 +1,21 @@
+// let index=1
+// while (index<=10){
+//     console.log(index);
+//     index=index+2
+// }
+
+// let myArray =["shaktiman","krish","hero"];
+// let index =0
+// while(index<myArray.length){
+//     console.log(myArray[index])
+//     index++
+// }
+
+//do while
+
+// let score =1
+let score =11
+do {
+    console.log(score)
+score++
+}while(score<=10)
