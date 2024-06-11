3a51d5d7231099bd9a854fa68bcd52db6d5b2861
udaydhanokar <139249387+udaydhanokar@users.noreply.github.com>
2024-06-11 18:16:52 +0000

    one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,48 @@
+//if
+isUserLoggedIn = true
+const tempreture = 41
+if (2==="2"){
+    console.log("executed")
+}
+
+//<,>,<=,>=,==,!=,===,!==
+
+if(tempreture<50){
+    console.log(`trempreture is ${tempreture}`)
+}else{
+    console.log("trepresture is greter than 50")
+}
+
+let score =200
+if (score===200){
+    let power ="fly"
+    console.log(`User power: ${power}`);
+}
+
+/*-------------------asa nahi karacha---------*/
+// const balance =1000;
+// if(balance>500) console.log('test'),console.log('test2');
+/*---------------------------------------------*/
+
+let balance =1000
+if(balance<500){
+    console.log("balance is less than 500")
+}
+else if(balance<750){
+    console.log("balance is less than 750")
+}
+else {
+    console.log("balance is greter than 750")
+}
+
+const UserLoggedIn = true
+const debitCard = true
+const isLoggedthrowGoogle = false
+const isLoggedthrowEmail = true
+if(UserLoggedIn && debitCard){
+    console.log("allow to buy course")
+}
+
+if(isLoggedthrowEmail||isLoggedthrowGoogle){
+    console.log("welcome to application")
+}
